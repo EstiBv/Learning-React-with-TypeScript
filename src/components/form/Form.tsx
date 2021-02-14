a4e25b5c2e8361f8714b4b221d75dcab549f2bb7
Estibaliz Barato <estibalizbarato@gmail.com>
2021-02-14 20:22:35 +0100

    section tasks ended

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 
@@ -19,23 +19,63 @@ const Form = () => {
   };
 
   // recibes tareas que son string
-  const addTask = (name: string) => {
+  const addTask = (name: string): void => {
     // añades las tareas y añades una nueva
     const newTasks: ITask[] = [...tasks, { name: name, done: false }];
     setTasks(newTasks);
   };
+
+  // btn toggle task
+  const handleToggleDoneTask = (index: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[index].done = !newTasks[index].done;
+    setTasks(newTasks);
+  };
+
+  const removeTask = (index: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="text">
+    <Fragment>
+      <form className="input-group mb-3" onSubmit={handleSubmit}>
         <input
+          className="form-control"
           type="text"
           id="text"
           onChange={(e) => setNewTask(e.target.value)}
           value={newTask}
+          autoFocus
         />
-      </label>
-      <button className="btn btn-outline-success">Save</button>
-    </form>
+        <button className="btn btn-outline-success">Save</button>
+      </form>
+      {tasks.map((task: ITask, index: number) => {
+        return (
+          <div key={index}>
+            <h6 style={{ textDecoration: task.done ? "line-through" : " " }}>
+              {task.name + " " + task.done}
+            </h6>
+            <small>
+              {task.done}
+              <button
+                className="btn btn-outline-primary btn-sm"
+                onClick={() => handleToggleDoneTask(index)}
+              >
+                {" "}
+                {task.done ? "✔︎" : "𐄂"}{" "}
+              </button>
+              <button
+                className="btn btn-outline-danger btn-sm"
+                onClick={() => removeTask(index)}
+              >
+                🗑
+              </button>
+            </small>
+          </div>
+        );
+      })}
+    </Fragment>
   );
 };
 
