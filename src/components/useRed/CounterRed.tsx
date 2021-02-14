903bb94c864f6e61924a1aa08d1608d43c4a25fd
Estibaliz Barato <estibalizbarato@gmail.com>
2021-02-14 12:33:16 +0100

    add useRed

--- src/components/useRed/CounterRed.tsx
+++ src/components/useRed/CounterRed.tsx
@@ -1,12 +1,13 @@
 import { useReducer } from "react";
-
+// defined state for reducer
 const initalState = {
   counter: 0,
 };
-// definir acciones
-type ActionCounterForReducer = {
-  type: "increase";
-};
+// defined actions for reducer
+type ActionCounterForReducer =
+  | { type: "increase" }
+  | { type: "decrease" }
+  | { type: "custom"; payload: number };
 
 /* retorna el estado, switch es el habitual. useReducer siempre retorna un  nuevo estado. Por prevención y defecto, retornar el state tras bucle */
 const counterReducer = (
@@ -19,6 +20,16 @@ const counterReducer = (
         ...state,
         counter: state.counter + 1,
       };
+    case "decrease":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "custom":
+      return {
+        ...state,
+        counter: action.payload,
+      };
     default:
       return state;
   }
@@ -27,6 +38,28 @@ const counterReducer = (
 const CounterRed = () => {
   // state + disparar acciones
   const [counterState, dispatch] = useReducer(counterReducer, initalState);
-  return <h4>Counter: {counterState.counter}</h4>;
+  return (
+    <>
+      <h4>Counter: {counterState.counter}</h4>
+      <button
+        className="btn btn-outline-warning btn-lg m-t2"
+        onClick={() => dispatch({ type: "increase" })}
+      >
+        +1
+      </button>
+      <button
+        className="btn btn-outline-warning btn-lg m-t2"
+        onClick={() => dispatch({ type: "decrease" })}
+      >
+        -1
+      </button>
+      <button
+        className="btn btn-outline-secondary btn-lg m-t2"
+        onClick={() => dispatch({ type: "custom", payload: 100 })}
+      >
+        100
+      </button>
+    </>
+  );
 };
 export default CounterRed;
